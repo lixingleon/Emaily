18c6c79dd378e2a7db3981900a5224f59a61a81a
lixingleon <tomatogo@outlook.com>
2021-08-20 16:25:16 -0700

    tweak ggoogle strategy settings

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,8 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy:true
         }, 
         //after the follow up request is made，this function is executed
         //(follow up requrest to google: passport automatically 
